7d1f8e7cbe07d9ff0ff31a211dd98aa431d3d703
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-04-16 13:07:42 -0400

    ranges

--- ranges..py
+++ ranges..py
@@ -1,1 +1,26 @@
-print(range(100))
+#
+# # list = list(range(10))
+# #
+# # print(list)
+#
+# even = list(range(0, 10, 2))
+# odd = list(range(1, 10, 2))
+#
+# print(even)
+# print(odd)
+#
+# string = "abcdefghijklmnopqrstuvwxyz"
+#
+# print(string.index("e"))
+# print(range(0,5,2) == range(0,6,2))
+
+o = range(0, 100, 4)
+print(o)
+
+for num in o:
+    print(num)
+
+p = o[::5]
+print(p)
+for i in p:
+    print(i)
